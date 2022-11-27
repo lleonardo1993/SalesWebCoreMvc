3a426d599c6b58b145df9f3bb479d805bf63a014
Leonardo <lleonardo1555@gmail.com>
2022-11-27 19:14:09 -0300

    CRUD Scaffolding

---
+++ SalesWebCore/Data/SalesWebCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebCore.Models;
+
+namespace SalesWebCore.Data
+{
+    public class SalesWebCoreContext : DbContext
+    {
+        public SalesWebCoreContext (DbContextOptions<SalesWebCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebCore.Models.Department> Department { get; set; }
+    }
+}
